dbecba21733f1f075a867e2b659f0dc7ab27de53
MHausmann97 <73695590+MHausmann97@users.noreply.github.com>
2020-10-30 11:42:48 +0100

    Add files via upload

---
+++ INF01_Uebung.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package inf01_uebung;
+
+/**
+ *
+ * @author marle
+ */
+public class INF01_Uebung {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
